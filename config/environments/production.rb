d022edad6e7b2d7c954ce3f772fa94b98c581ef3
Ted Healey <thealey@gmail.com>
2011-08-31 08:31:09 -0400

    Got Devise emails working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,14 @@ Slim::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'slim.flunkyism.com' }
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  :address              => "localhost",
+  :port                 => 25,
+  :domain               => 'li328-252.members.linode.com',
+  :enable_starttls_auto => false
+}
 end
