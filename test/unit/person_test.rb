9c8c21d6d04231bd16f00593596e40e2d7062c15
Ted Healey <thealey@gmail.com>
2011-09-01 11:52:47 -0400

    Auto test work

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -6,6 +6,9 @@ class PersonTest < ActiveSupport::TestCase
     attributes[:email] ||= 'foo@example.com'
     attributes[:password] ||= 'abc123'
     attributes[:password_confirmation] ||= attributes[:password]
+    attributes[:goal] ||= '190'
+    attributes[:height_feet] ||= '6'
+    attributes[:inches] ||= '4'
     person = Person.new(attributes)
     person.valid? # run validations
     person
@@ -24,7 +27,7 @@ class PersonTest < ActiveSupport::TestCase
   end
 
   def test_require_password
-    assert_equal ["can't be blank"], new_person(:password => '').errors[:password]
+    assert_equal ["can't be blank","can't be blank"], new_person(:password => '').errors[:password]
   end
 
   def test_require_well_formed_email
@@ -36,51 +39,15 @@ class PersonTest < ActiveSupport::TestCase
     assert_equal ["has already been taken"], new_person(:email => 'bar@example.com').errors[:email]
   end
 
-  def test_validate_uniqueness_of_username
-    new_person(:username => 'uniquename').save!
-    assert_equal ["has already been taken"], new_person(:username => 'uniquename').errors[:username]
-  end
-
   def test_validate_odd_characters_in_username
     assert_equal ["should only contain letters, numbers, or .-_@"], new_person(:username => 'odd ^&(@)').errors[:username]
   end
 
   def test_validate_password_length
-    assert_equal ["is too short (minimum is 4 characters)"], new_person(:password => 'bad').errors[:password]
+    assert_equal ["is too short (minimum is 4 characters)","is too short (minimum is 4 characters)"], new_person(:password => 'bad').errors[:password]
   end
 
   def test_require_matching_password_confirmation
-    assert_equal ["doesn't match confirmation"], new_person(:password_confirmation => 'nonmatching').errors[:password]
-  end
-
-  def test_generate_password_hash_and_salt_on_create
-    person = new_person
-    person.save!
-    assert person.password_hash
-    assert person.password_salt
-  end
-
-  def test_authenticate_by_username
-    Person.delete_all
-    person = new_person(:username => 'foobar', :password => 'secret')
-    person.save!
-    assert_equal person, Person.authenticate('foobar', 'secret')
-  end
-
-  def test_authenticate_by_email
-    Person.delete_all
-    person = new_person(:email => 'foo@bar.com', :password => 'secret')
-    person.save!
-    assert_equal person, Person.authenticate('foo@bar.com', 'secret')
-  end
-
-  def test_authenticate_bad_username
-    assert_nil Person.authenticate('nonexisting', 'secret')
-  end
-
-  def test_authenticate_bad_password
-    Person.delete_all
-    new_person(:username => 'foobar', :password => 'secret').save!
-    assert_nil Person.authenticate('foobar', 'badpassword')
+    assert_equal ["doesn't match confirmation","doesn't match confirmation"], new_person(:password_confirmation => 'nonmatching').errors[:password]
   end
 end
